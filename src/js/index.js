cf254e2632248c2313277f68dc3970aeb6e3a7af
Omrishoham <omri.s13@gmail.com>
2020-07-06 12:55:15 +0300

    adding html,css code

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-//this is the controller
+//this is our controller
 import Search from './models/Search'
 
+/*Global state of the app
+*-Search object
+*-Current recipe object
+*-Shopping list object
+*-Like recipes
+*/
+
+
+const state = {}; 
 const search = new Search('chicken');
 search.getResults();
 
