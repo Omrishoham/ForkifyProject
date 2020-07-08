8355c1f148873b447eaa4947fdb227ed867b5a49
Omrishoham <omri.s13@gmail.com>
2020-07-08 03:33:47 +0300

    adding methods to model and view

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,42 @@
 //this is our controller
 import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
 
 /*Global state of the app
 *-Search object
 *-Current recipe object
 *-Shopping list object
 *-Like recipes
+(this object is global and we can change him from the functions)
 */
-
-
 const state = {}; 
-const search = new Search('chicken');
+
+//function that we call when event submit on search form
+//remember: async function returns a promise
+//remember:if we wanna use await we need to make the func async
+const controlSearch = async ()=>{
+    //1) get query from view
+    const query = searchView.getInput(); 
+    if(query){
+        //2)New search object and add to state
+        state.search = new Search(query);
+        //3)prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4)search for recipes
+        await state.search.getResults();
+
+        //5)render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit',event=>{
+    event.preventDefault();
+    controlSearch();
+});
+
 search.getResults();
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,5 +1,35 @@
+import { elements } from './base'
+//here we export a function we will use to get the input the client write
+export const getInput = () => elements.searchInput.value;//return the value
 
+export const clearInput = () => {
+    elements.searchInput.value = '';//(not returning with braces)
+}
 
+export const clearResults = ()=>{
+    elements.searchResultList.innerHTML = '';
+}
+//this function will render the recipe where it needs to be in the html code
+const renderRecipe = (recipe)=>{
+    const markup = `
+    <li>
+        <a class="results__link" href="${recipe.recipe_id}">
+           <figure class="results__fig">
+              <img src="${recipe.image_url}" alt="Test">
+            </figure>
+            <div class"results__data">
+               <h4 class="results__name">${recipe.title}</h4>
+               <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>           
+    `;
+    elements.searchResultList.insertAdjacentHTML('beforeend',markup);//a func that insert the html code exactly where we want with the specific location
+};
+
+export const renderResults = (recipes) => {
+    recipes.forEach(recipe=>renderRecipe(recipe));
+};
 
 
 
